199188a9be64a81eaa4864ef029869ce53cfe788
Umair513 <soomroumair513@gmail.com>
2024-04-02 12:10:59 +0500

    user model and hash password

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import coonnectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
 
 dotenv.config();
-coonnectDB()
+coonnectDB();
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api/v1/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send({
