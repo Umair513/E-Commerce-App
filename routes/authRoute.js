5c9feaaa68ac2fd7321f7a71482736c8d1cabba7
Umair513 <soomroumair513@gmail.com>
2024-04-04 10:35:24 +0500

    protected middleware

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { registerController } from "../controllers/authController.js";
 import { testController } from "../controllers/authController.js";
-import { requireSignIn } from "../middlewares/authMiddleware.js";
+import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import { loginController } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
-router.get("/test", requireSignIn, testController);
+router.get("/test", requireSignIn,isAdmin, testController);
 export default router;
