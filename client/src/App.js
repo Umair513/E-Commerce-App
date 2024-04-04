864b040ab3d770b4374b71741116150d4aecbe35
Umair513 <soomroumair513@gmail.com>
2024-04-04 17:26:37 +0500

    React app, components, pages, routing

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Layout from "./components/Layout/Layout";
+import Header from "./components/Layout/Header";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage></HomePage>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/policy" element={<Policy></Policy>}></Route>
+        <Route path="*" element={<PageNotFound></PageNotFound>}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
