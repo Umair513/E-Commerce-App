63eb06b998b29e94a7082fb7ad41288e92a7d551
Umair513 <soomroumair513@gmail.com>
2024-05-21 11:32:19 +0500

    user order list

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 import { comparePassword, hashPassword } from "../helpers/authHelper.js";
 import userModel from "../models/userModel.js";
 import JWT from "jsonwebtoken";
+import orderModel from "../models/orderModel.js"
+
 export const registerController = async (req, res) => {
   try {
     const { name, email, password, phone, address, answer } = req.body;
@@ -169,6 +171,20 @@ export const updateProfileController = async (req, res)  => {
     res.status(400).send({
       success: false,
       message:"Error while update profile",
+      error
+    })
+  }
+}
+
+export const getOrdersController = async  (req, res) => {
+  try {
+    const orders = await orderModel.find({buyer: req.user._id}).populate("products", "-photo").populate("buyer", "name")
+    res.json(orders)   
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      success: false,
+      message: "error while getting orders",
       error
     })
   }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { forgotPasswordController, registerController } from "../controllers/authController.js";
+import { forgotPasswordController, getOrdersController, registerController } from "../controllers/authController.js";
 import { testController } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import { loginController } from "../controllers/authController.js";
@@ -18,4 +18,5 @@ router.get("/admin-auth", requireSignIn,isAdmin, (req, res) => {
     res.status(200).send({ok: true})
 })
 router.put("/profile", requireSignIn, updateProfileController)
+router.get("/orders", requireSignIn, getOrdersController)
 export default router;
